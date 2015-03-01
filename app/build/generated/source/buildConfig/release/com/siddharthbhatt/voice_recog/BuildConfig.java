d5771861042cabb7c61d4d9466f380de1bab313e
sidbhatt11 <sidbhatt11@yahoo.in>
2015-03-02 01:29:47 +0530

    Complete App Source

---
+++ app/build/generated/source/buildConfig/release/com/siddharthbhatt/voice_recog/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.siddharthbhatt.voice_recog;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.siddharthbhatt.voice_recog";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
